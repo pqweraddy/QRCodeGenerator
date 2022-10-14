35ff963d7b77bf2301659c768d8818acb4897d45
Paddy <pqweraddy@gmail.com>
2022-10-14 09:37:58 +0800

    Update qr.py
    added more vcard contents

--- qr.py
+++ qr.py
@@ -5,7 +5,7 @@ sg.theme('LightGreen')
 
 
 layout1 = [
-          [sg.Text('choose how you want to be', size=(40, 1), justification='left')],
+          [sg.Text('Select Type', size=(20, 1), justification='center')],
           [sg.B('Hyperlink', 
            size=(10, 1),key='text',enable_events=True,button_color='red')],
           [sg.B('Contacts',
@@ -19,18 +19,24 @@ layout2 = [
         [sg.Text('Last Name', size=(10, 1)),sg.Input(size=(30,1),key='ln')],
         [sg.Text('Telephone', size=(10, 1)),sg.I(size=(30,1),key='tel')],
         [sg.Text('email', size=(10, 1)),sg.I(size=(30,1),key='email')],
-        [sg.B('Generate',
-         size=(10, 1),key='vcok',enable_events=True,button_color='red')]
+        [sg.Text('website', size=(10, 1)),sg.I(size=(30,1),key='url')],        
+        [sg.Text('Street', size=(10, 1)),sg.I(size=(30,1),key='st')],
+        [sg.Text('City', size=(10, 1)),sg.I(size=(12,1),key='city'), 
+         sg.Text('ZIP Code', size=(8, 1)),sg.I(size=(7,1),key='zi')], 
+        [sg.Text('State', size=(10, 1)),sg.I(size=(30,1),key='state')],
+        [sg.Text('Country', size=(10, 1)),sg.I(size=(30,1),key='coun')], 
+        [sg.B('Generate',size=(10, 1),key='vcok',enable_events=True,button_color='red'),
+         sg.B('Cancel',size=(10, 1),key='cancel2',enable_events=True,button_color='gray')]
     ]
 
 layout3 = [
         [sg.Text('Website', size=(10, 1)),sg.I(size=(30,1),key='web',default_text="https://")],
-        [sg.B('Generate',
-         size=(10, 1),key='webok',enable_events=True,button_color='red')]
+        [sg.B('Generate',size=(10, 1),key='webok',enable_events=True,button_color='red'),
+         sg.B('Cancel',size=(10, 1),key='cancel3',enable_events=True,button_color='gray')]
     ]
 
 layout = [[sg.Column(layout1, key='-COL1-'), sg.Column(layout2, visible=False, key='-COL2-'), sg.Column(layout3, visible=False, key='-COL3-')],
-          [sg.Text('A Paddy Lee Production', size=(40, 1), justification='center')]]
+          [sg.Text('A Paddy Lee Production', size=(20, 1), justification='center')]]
 
 
 
@@ -39,16 +45,22 @@ layout = [[sg.Column(layout1, key='-COL1-'), sg.Column(layout2, visible=False, k
 window = sg.Window('QR Code Generator', layout)
 str = ""
 
-# 消息循环
+# 事件循環
 while True:
     event, values = window.read()
-    # print(event,values)
     if event == 'Exit' or event == sg.WIN_CLOSED:
         break
     elif event == 'text':
         window[f'-COL1-'].update(visible=False)
         window[f'-COL3-'].update(visible=True)
         
+    elif event == 'cancel2':
+        window[f'-COL2-'].update(visible=False)
+        window[f'-COL1-'].update(visible=True)
+
+    elif event == 'cancel3':
+        window[f'-COL3-'].update(visible=False)
+        window[f'-COL1-'].update(visible=True)
         
     elif event == 'vcard':
         window[f'-COL1-'].update(visible=False)
@@ -68,7 +80,13 @@ while True:
         fn = values['fn']
         ln = values['ln']
         tel = values['tel']
+        st = values['st']
+        city = values['city']
+        state = values['state']
+        coun = values['coun']
         email = values['email']
+        url = values['url']
+        zi = values['zi']
         
         
         template = """
@@ -76,10 +94,12 @@ BEGIN:VCARD
 VERSION:3.0
 N:{};{}
 FN:{} {}
+ADR:;;{};{};{};{};{}
 TEL;CELL:{}
 EMAIL;WORK;INTERNET:{}
+URL:{}
 END:VCARD
-""".format(ln,fn,fn,ln,tel,email)
+""".format(ln,fn,fn,ln,st,city,state,zi,coun,tel,email,url)
         img = qrcode.make(template)
         type(img)  # qrcode.image.pil.PilImage
         img.save("some_file.png")
