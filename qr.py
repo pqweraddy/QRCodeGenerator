80d72ef58f5fbbf1fb6c5dc1e422a64c5cbb88f0
Paddy <pqweraddy@gmail.com>
2022-10-17 09:55:38 +0800

    Update qr.py
    add text and some easter eggs

--- qr.py
+++ qr.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Oct 15 20:33:53 2022
-
+Created on Sat Oct 17 09:45:53 2022
 @author: Paddy
+install qrcode pillow pysimplegui
 """
 
 import PySimpleGUI as sg
 import qrcode
+import webbrowser
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers import VerticalBarsDrawer,RoundedModuleDrawer,HorizontalBarsDrawer,SquareModuleDrawer,GappedSquareModuleDrawer,CircleModuleDrawer
 qr = qrcode.QRCode(
     version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_M,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
     box_size=10,
     border=4
 )
@@ -21,13 +22,17 @@ sg.theme('Dark')
 layout1 = [
           [sg.Text('Select Type', size=(20, 1), justification='center')],
           [sg.B('Hyperlink', 
-           size=(10, 1),key='text',enable_events=True,button_color='red')],
-          [sg.B('Contacts',
-           size=(10, 1),key='vcard',enable_events=True,button_color='red')],
+           size=(10, 1),key='toweb',enable_events=True,button_color='red'),
+           sg.B('Contacts',
+           size=(10, 1),key='tovcard',enable_events=True,button_color='red')],
+          [sg.B('Text', 
+           size=(10, 1),key='totext',enable_events=True,button_color='red'),
+           sg.B('View Author',
+           size=(10, 1),key='view',enable_events=True,button_color='gray')],
           [sg.Exit()],
 
     ]
-layout2 = [
+layout2 = [ #vcard
         [sg.Text('Input Your Information', size=(40, 1), justification='center')],
         [sg.Text('First Name', size=(10, 1)),sg.InputText(size=(30,1),key='fn')],
         [sg.Text('Last Name', size=(10, 1)),sg.Input(size=(30,1),key='ln')],
@@ -39,34 +44,51 @@ layout2 = [
          sg.Text('ZIP Code', size=(8, 1)),sg.I(size=(7,1),key='zi')], 
         [sg.Text('State', size=(10, 1)),sg.I(size=(30,1),key='state')],
         [sg.Text('Country', size=(10, 1)),sg.I(size=(30,1),key='coun')],
-        [sg.Text('choose your style', size=(40, 1), justification='center')],
-        [sg.Radio('Default','style',key='stylev_default',default=True,size=(10,1)),
-         sg.Radio('小方格','style',key='stylev_2',default=False,size=(10,1)),
-         sg.Radio('圓點','style',key='stylev_3',default=False,size=(10,1))],
-        [sg.Radio('圓角方格','style',key='stylev_4',default=False,size=(10,1)),
-         sg.Radio('直線','style',key='stylev_5',default=False,size=(10,1)),
-         sg.Radio('橫線','style',key='stylev_6',default=False,size=(10,1)),],
+        [sg.Text('Choose Your Style', size=(40, 1), justification='center')],
+        [sg.Radio('Default','stylev',key='stylev_default',default=True,size=(10,1)),
+         sg.Radio('小方格','stylev',key='stylev_2',default=False,size=(10,1)),
+         sg.Radio('圓點','stylev',key='stylev_3',default=False,size=(10,1))],
+        [sg.Radio('圓角方格','stylev',key='stylev_4',default=False,size=(10,1)),
+         sg.Radio('直線','stylev',key='stylev_5',default=False,size=(10,1)),
+         sg.Radio('橫線','stylev',key='stylev_6',default=False,size=(10,1)),],
         [sg.Text("Select Logo: "), sg.FileBrowse(key="v_in")],
-        [sg.B('Generate',size=(10, 1),key='vcok',enable_events=True,button_color='red'),
+        [sg.B('Generate',size=(20, 1),key='vcok',enable_events=True,button_color='red'),
          sg.B('Cancel',size=(10, 1),key='cancel2',enable_events=True,button_color='gray')]
     ]
 
-layout3 = [
+layout3 = [ #web
         [sg.Text('Input Your Website', size=(40, 1), justification='center')],
         [sg.Text('Website', size=(10, 1)),sg.I(size=(30,1),key='web',default_text="https://")],
         [sg.Text('Choose Your Style', size=(40, 1), justification='center')],
-        [sg.Radio('Default','style',key='stylew_default',default=True,size=(10,1)),
-         sg.Radio('小方格','style',key='stylew_2',default=False,size=(10,1)),
-         sg.Radio('圓點','style',key='stylew_3',default=False,size=(10,1))],
-        [sg.Radio('圓角方格','style',key='stylew_4',default=False,size=(10,1)),
-         sg.Radio('直線','style',key='stylew_5',default=False,size=(10,1)),
-         sg.Radio('橫線','style',key='stylew_6',default=False,size=(10,1)),],
+        [sg.Radio('Default','stylew',key='stylew_default',default=True,size=(10,1)),
+         sg.Radio('小方格','stylew',key='stylew_2',default=False,size=(10,1)),
+         sg.Radio('圓點','stylew',key='stylew_3',default=False,size=(10,1))],
+        [sg.Radio('圓角方格','stylew',key='stylew_4',default=False,size=(10,1)),
+         sg.Radio('直線','stylew',key='stylew_5',default=False,size=(10,1)),
+         sg.Radio('橫線','stylew',key='stylew_6',default=False,size=(10,1)),],
         [sg.Text("Select Logo: "), sg.FileBrowse(key="w_in")],
-        [sg.B('Generate',size=(10, 1),key='webok',enable_events=True,button_color='red'),
+        [sg.B('Generate',size=(20, 1),key='webok',enable_events=True,button_color='red'),
          sg.B('Cancel',size=(10, 1),key='cancel3',enable_events=True,button_color='gray')]
     ]
+layout4 = [ #text
+        [sg.Text('Input Your Text', size=(40, 1), justification='center')],
+        [sg.Text('Text', size=(10, 1)),sg.I(size=(30,1),key='text')],
+        [sg.Text('Choose Your Style', size=(40, 1), justification='center')],
+        [sg.Radio('Default','stylet',key='stylet_default',default=True,size=(10,1)),
+         sg.Radio('小方格','stylet',key='stylet_2',default=False,size=(10,1)),
+         sg.Radio('圓點','stylet',key='stylet_3',default=False,size=(10,1))],
+        [sg.Radio('圓角方格','stylet',key='stylet_4',default=False,size=(10,1)),
+         sg.Radio('直線','stylet',key='stylet_5',default=False,size=(10,1)),
+         sg.Radio('橫線','stylet',key='stylet_6',default=False,size=(10,1)),],
+        [sg.Text("Select Logo: "), sg.FileBrowse(key="t_in")],
+        [sg.B('Generate',size=(20, 1),key='textok',enable_events=True,button_color='red'),
+         sg.B('Cancel',size=(10, 1),key='cancel4',enable_events=True,button_color='gray')]
+    ]
 
-layout = [[sg.Column(layout1, key='-COL1-'), sg.Column(layout2, visible=False, key='-COL2-'), sg.Column(layout3, visible=False, key='-COL3-')],
+
+
+layout = [[sg.Column(layout1, key='-COL1-'), sg.Column(layout2, visible=False, key='-COL2-'), 
+           sg.Column(layout3, visible=False, key='-COL3-'), sg.Column(layout4, visible=False, key='-COL4-')],
           [sg.Text('A Paddy Lee Production', size=(20, 1), justification='center')]]
 
 
@@ -82,10 +104,19 @@ while True:
     event, values = window.read()
     if event == 'Exit' or event == sg.WIN_CLOSED:
         break
-    elif event == 'text':
+    
+    elif event == 'tovcard':
+        window['-COL1-'].update(visible=False)
+        window['-COL2-'].update(visible=True)
+        
+    elif event == 'toweb':
         window['-COL1-'].update(visible=False)
         window['-COL3-'].update(visible=True)
         
+    elif event == 'totext':
+        window['-COL1-'].update(visible=False)
+        window['-COL4-'].update(visible=True)
+
     elif event == 'cancel2':
         window['-COL2-'].update(visible=False)
         window['-COL1-'].update(visible=True)
@@ -94,10 +125,16 @@ while True:
         window['-COL3-'].update(visible=False)
         window['-COL1-'].update(visible=True)
         
-    elif event == 'vcard':
-        window['-COL1-'].update(visible=False)
-        window['-COL2-'].update(visible=True)
-            
+    elif event == 'cancel4':
+        window['-COL4-'].update(visible=False)
+        window['-COL1-'].update(visible=True)
+        
+        
+    elif event == 'view':
+        webbrowser.open("https://youtu.be/dQw4w9WgXcQ")
+        sg.popup("you got rickroll'd",title="lol")
+
+
     elif event == 'webok':
         web = values['web'];
         path = r"{}".format(values['w_in']);
@@ -127,16 +164,42 @@ while True:
             img = qr.make_image(image_factory=StyledPilImage, module_drawer=HorizontalBarsDrawer(),embeded_image_path=path)
             img.save("some_file.png")
             img.show()
-        else:
-            sg.popup("Please Select Your Style")
         window['-COL3-'].update(visible=False)
         window['-COL1-'].update(visible=True)
-        
-           # 要轉換成 QRCode 的文字
-          # 根據參數製作為 QRCode 物件
 
-      # 產生 QRCode 圖片
         
+    elif event == 'textok':
+        web = values['text'];
+        path = r"{}".format(values['t_in']);
+        qr.add_data(web)
+        qr.make(fit=True)
+        if values["stylet_default"] == True:
+            img = qr.make_image(image_factory=StyledPilImage, module_drawer=SquareModuleDrawer(),embeded_image_path=path)
+            img.save("some_file.png")
+            img.show()
+        elif values["stylet_2"] == True:
+            img = qr.make_image(image_factory=StyledPilImage, module_drawer=GappedSquareModuleDrawer(),embeded_image_path=path)
+            img.save("some_file.png")
+            img.show()
+        elif values["stylet_3"] == True:
+            img = qr.make_image(image_factory=StyledPilImage, module_drawer=CircleModuleDrawer(),embeded_image_path=path)
+            img.save("some_file.png")
+            img.show()
+        elif values["stylet_4"] == True:
+            img = qr.make_image(image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer(),embeded_image_path=path)
+            img.save("some_file.png")
+            img.show()
+        elif values["stylet_5"] == True:
+            img = qr.make_image(image_factory=StyledPilImage, module_drawer=VerticalBarsDrawer(),embeded_image_path=path)
+            img.save("some_file.png")
+            img.show()
+        elif values["stylet_6"] == True:
+            img = qr.make_image(image_factory=StyledPilImage, module_drawer=HorizontalBarsDrawer(),embeded_image_path=path)
+            img.save("some_file.png")
+            img.show()
+        window['-COL4-'].update(visible=False)
+        window['-COL1-'].update(visible=True)
+
     elif event == 'vcok':
         path = r"{}".format(values['v_in']);
         fn = values['fn']
@@ -187,9 +250,7 @@ END:VCARD
             img = qr.make_image(image_factory=StyledPilImage, module_drawer=HorizontalBarsDrawer(),embeded_image_path=path)
             img.save("some_file.png")
             img.show()
-        else:
-            sg.popup("Please Select Your Style")
         window['-COL2-'].update(visible=False)
         window['-COL1-'].update(visible=True)
-        
+             
 window.close()
